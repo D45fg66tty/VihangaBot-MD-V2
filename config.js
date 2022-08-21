381c37d7cb02242e35cc9f0fcb75503b0dd955f5
D45fg66tty <108977786+D45fg66tty@users.noreply.github.com>
2022-08-21 11:35:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -18,14 +18,14 @@ global.APIKeys = {
 
 //Owner React
 global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දාන්න.
-global.REACT_NUMBER = '9476342733' //මෙතන number එක.
+global.REACT_NUMBER = '94762566949' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
+global.owner = ['94762566949',''] //මෙතන number එක.
+global.premium = ['94762566949','9476342733'] //මෙතන number එක.
+global.botname = 'DINUSHA BOT INC' //මෙතන නම.
+global.ownername = ' dinusha MD' //මෙතන නම.
+global.ownername = 'DINUSHA MD' //මෙතන නම.
 global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
 global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
 global.packname = 'VIHANGA BOT INC' //මෙතන නම.
